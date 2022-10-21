0e712c79c5202afea7c4d91b806da966ff23aae0
domino659 <martin@sionfamily.com>
2022-10-22 01:41:13 +0200

    first page

---
+++ src/src/Controller/IndexController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(): Response
+    {
+        return $this->render('index/index.html.twig');
+    }
+}
+// {
+//     /**
+//      * @Route("/")
+//      * @return Response
+//      */
+//     public function index()
+//     {
+//         return new Response('Hello World!');
+//     }
+// }
