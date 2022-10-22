494b0291dc0a0cdd5f135c1e25fab97058f75a91
domino659 <martin@sionfamily.com>
2022-10-23 01:45:07 +0200

    installerd maker

--- src/config/bundles.php
+++ src/config/bundles.php
@@ -9,4 +9,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Knp\Bundle\MarkdownBundle\KnpMarkdownBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
